efb1b791ac21fcacda221440dc4aaf0d4358f216
Ezequielpereyraa <ezequielpereyra@hotmail.com>
2020-09-23 22:29:39 -0300

    finish homework 7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -9,18 +9,24 @@ function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  cb(numeros);
+  const total = numeros.reduce((acc, curr) => {
+    return acc + curr;
+  });
+  cb(total);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach((arr) => cb(arr));
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  const nuevoArray = array.map((arr) => cb(arr));
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
